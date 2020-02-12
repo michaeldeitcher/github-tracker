dfd9c7235efcbae6385058e253f0458328966373
Michael Deitcher <michaeldeitcher@gmail.com>
2020-02-12 10:50:11 -0700

    Trackable property refactor
    It's better to use trackable properties instead of POJO(Plain Old Javascript) objects.

--- app/components/github-events-query.js
+++ app/components/github-events-query.js
@@ -1,22 +1,36 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
+import { A } from '@ember/array';
 import { task } from 'ember-concurrency';
+
+class EventData {
+  @tracked type;
+  @tracked actorName;
+  @tracked createdAt;
+}
 
 export default class GithubEventsQuery extends Component {
   @tracked owner = "michaeldeitcher";
   @tracked repo = "force-graph-three";
-  @tracked eventType = "";
+  @tracked eventTypes = A([]);
 
-  @(task(function*() {
-    let response = yield fetch(`https://api.github.com/repos/${this.owner}/${this.repo}/events`);
-    let json = yield response.json();
-    this.args.onEventsReceived(json);
-  }).restartable()) fetchEvents;
+  @task(function*() {
+    let response = yield  fetch(`https://api.github.com/repos/${this.owner}/${this.repo}/events`);
+    let json =  yield response.json();
+    let eventData = A([]);
+    json.forEach( element => {
+      let e = new EventData();
+      e.type = element.type;
+      e.actorName = element.actor.login;
+      e.createdAt = element.created_at;
+      eventData.pushObject(e);
+    });
+    this.args.onEventsReceived(eventData);
+  }) fetchEvents;
 
   @action
   updateOwner(value) {
-    window.console.info(value);
     this.owner = value;
   }
 
